23fd02531b6313306bdc1d4c32a847b0442b51f0
Prateek Gangwar <prateek497@gmail.com>
2018-02-10 10:20:16 +0530

    Add project files.

---
+++ TigerspikeAssignment.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TigerspikeAssignment.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
